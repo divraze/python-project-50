526aca51c82e99515b8bfd6d9b63138820e86454
divraze <divraze@mail.ru>
2025-01-14 17:45:10 +0400

    Fixed files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,13 @@
 #!usr/bin/env python3
 import argparse
+
 from gendiff.load_files import generate_diff
 
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference.")
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument("-f", "--format", help="set format of output")
